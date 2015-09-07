267362df84e5b581ecf4b8d5e1e8cd6373d24c98
Rachee <rachee.singh@gmail.com>
2015-09-06 21:43:43 -0400

    barebones

---
+++ bin/sbush/sbush.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_BUF_SIZE 200
+#define MAX_CMD_LEN 50
+#define MAX_ARGS 5
+#define MAX_ARG_LEN 30
+#define MAX_ENV_VARS 2
+
+/*
+getcmd takes unprocessed user input (buf) and put the command in the
+variable "cmd". The index from which user arguments begin in buf is returned
+from the function as "index".
+*/
+int getcmd( char* buf, char* cmd ) {
+  int index = 0;
+
+  // Remove initial whitespaces
+  while( *buf == ' ' )
+    buf++;
+  
+  while( *buf != '\0' ) {
+    if ( *buf == ' ' ) {
+      break;
+    }
+    cmd[index] = *buf;
+    index++;
+    buf++;    
+  }
+  return index;
+}
+
+/*
+getargs takes off from getcmd finished. It looks at buf from the index
+returned by getcmd. So, getargs looks only at userargs and returns
+an array of strings containing arguments to the command entered by the user.
+*/
+char** getargs( char* buf ) {
+  char** args;
+  int argnum = 0, count = 0, i;
+
+  args = malloc( MAX_ARGS * sizeof( char* ) );
+  for( i=0; i<MAX_ARGS; i++)
+    args[ i ] = malloc( MAX_ARG_LEN * sizeof( char ) );
+  
+  while( *buf == ' ' )
+    buf++;
+
+  while( *buf!= '\0' ) {
+    if ( *buf == ' ' ) {
+      argnum++;
+      count = 0;
+      buf++;
+    }
+    args[ argnum ][ count ] = *buf;
+    buf++;
+    count++;
+  }
+  return args;
+}
+
+int main() {
+  char buf[MAX_BUF_SIZE];
+  char cmd[MAX_CMD_LEN];
+  char** args;
+  char** envp = NULL;
+  int argindex, pid;
+  int i;
+  cmd[ 0 ] = 0;
+
+  while ( 1 ) {    
+    printf( "\nsbush>" );
+    gets( buf );
+    argindex = getcmd( buf, cmd );
+    
+    args = getargs( &buf[ argindex ] );
+
+    pid = fork();
+    if ( pid == 0 ) {
+      printf( "In child..\n" );
+      execve( cmd, args, envp);
+      printf( "Exec failed!\n" );
+      exit( 0 );
+    } else if ( pid > 0 ) {
+      printf( "In parent..\n" );
+      waitpid( pid, 0, 0 );
+      printf( "Child Exited\n" );
+    } else {
+      printf( "FORK ERROR, EXITING..\n" );
+      exit( 1 );
+    }
+    // Trying not to use memset so resetting the buffer manually
+    for ( i=0; i<MAX_BUF_SIZE; i++ )
+      buf[ i ] = 0;
+  }
+  return 0;
+}
--- include/stdio.h
+++ include/stdio.h
@@ -2,5 +2,6 @@
 #define _STDIO_H
 
 int printf(const char *format, ...);
-
+int scanf(const char *format, ...);
+char *gets(char *str);
 #endif
