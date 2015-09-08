92ed5bca4d5626be03c0a6534ca5fc48e7eca309
Emaad Ahmed Manzoor <emaadmanzoor@gmail.com>
2015-09-08 12:46:16 -0400

    implement strncpy, strenghten string tests

--- bin/tests/test_strcpy.c
+++ bin/tests/test_strcpy.c
@@ -6,9 +6,11 @@ void test_strcpy() {
   printf("strcpy tests start.\n");
   
   char *dest = (char *) malloc(sizeof(char) * 5);
+  dest[4] = 'E'; // should be overwritten by null
   strcpy(dest, "abcd");
   if (strcmp(dest, "abcd") != 0)
       printf("strcpy test 1 failed\n");
+  free(dest);
 
-  printf("strlen tests done.\n");
+  printf("strcpy tests done.\n");
 }
