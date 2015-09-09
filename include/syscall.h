d9e9e2b1248c2e686aaed84dc63512ce07af5a0a
Rachee <rachee.singh@gmail.com>
2015-09-09 15:18:17 -0400

    gettign exit to work

--- include/syscall.h
+++ include/syscall.h
@@ -4,12 +4,16 @@
 #include <sys/defs.h>
 #include <sys/syscall.h>
 
+long (__syscall)(long, ...);
+
 static __inline int64_t syscall_0(uint64_t n) {
 	return 0;
 }
 
 static __inline int64_t syscall_1(uint64_t n, uint64_t a1) {
-	return 0;
+        unsigned long ret;
+        __asm__ __volatile__ ("syscall" : "=a"(ret) : "a"(n), "D"(a1) : "rcx", "r11", "memory");
+	return ret;
 }
 
 static __inline int64_t syscall_2(uint64_t n, uint64_t a1, uint64_t a2) {
