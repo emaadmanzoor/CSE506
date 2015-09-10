63abee583fd7626296967ff95dd5ed8dda1f5f0a
Emaad Ahmed Manzoor <emaadmanzoor@gmail.com>
2015-09-10 03:53:01 -0400

    fix permissions, formatting

--- libc/printf.c
+++ libc/printf.c
@@ -5,14 +5,14 @@
 int printf(const char *format, ...) {
   va_list val;
   int printed = 0;
-  
+
   va_start(val, format);
-  
+
   while(*format) {
     write(1, format, 1);
     ++printed;
     ++format;
   }
-  
+
   return printed;
 }
