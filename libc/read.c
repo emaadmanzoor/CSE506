3801a7910802740373498df955b852ef6178d9ad
Emaad Ahmed Manzoor <emaadmanzoor@gmail.com>
2015-09-10 03:43:10 -0400

    minor formatting

--- libc/read.c
+++ libc/read.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include "syscall.h"
 
-ssize_t read(int fd, void *buf, size_t count)
-{
-  return syscall_3(SYS_read, fd, (long) buf, (long)count);
+ssize_t read(int fd, void *buf, size_t count) {
+  return syscall_3(SYS_read, fd, (long) buf, (long) count);
 }
