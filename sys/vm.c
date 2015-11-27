77849ced638f2051d46f9a2bc4ec62867071fd8d
Emaad Ahmed Manzoor <emaadmanzoor@gmail.com>
2015-11-27 16:46:55 -0500

    add page mapping function

--- sys/vm.c
+++ sys/vm.c
@@ -1,7 +1,7 @@
 #include <sys/sbunix.h>
 #include <sys/asm.h>
 
-int setupkvm(uint64_t physend) {
+pte_t* setupkvm(uint64_t physend) {
   // page table management pointers
   pte_t *pml4;
   pte_t *pdpt, *pdt, *pt;
@@ -16,6 +16,7 @@ int setupkvm(uint64_t physend) {
   if ((pml4 = (pte_t*) kalloc()) == 0) {
     return 0; // error allocating a page for the PML4
   }
+  memset(pml4, 0, PGSIZE);
 
   // map va -> pa in each iteration of this loop,
   // and increment va and pa by +PGSIZE.
@@ -25,7 +26,7 @@ int setupkvm(uint64_t physend) {
     // Level 1. Map the PDPT entry in PML4E
     pml4e = &pml4[PML4_INDEX(va)];
     if (*pml4e & PTE_P) {
-      pdpt = (pte_t*) P2V(*pml4e & ~(0x7));
+      pdpt = (pte_t*) P2V(*pml4e & ~(0xfff));
     } else {
 
       if ((pdpt = (pte_t*) kalloc() ) == 0) {
@@ -39,7 +40,7 @@ int setupkvm(uint64_t physend) {
     // Level 2. Map the PDT entry in PDPT
     pdpte = &pdpt[PDPT_INDEX(va)];
     if (*pdpte & PTE_P) {
-      pdt = (pte_t*) P2V(*pdpte & ~(0x7));
+      pdt = (pte_t*) P2V(*pdpte & ~(0xfff));
     } else {
 
       if((pdt = (pte_t*) kalloc()) == 0) {
@@ -53,7 +54,7 @@ int setupkvm(uint64_t physend) {
     // Level 3. Map the PT entry in PDT
     pde = &pdt[PDT_INDEX(va)];
     if (*pde & PTE_P) {
-      pt = (pte_t*) P2V(*pde & ~(0x7));
+      pt = (pte_t*) P2V(*pde & ~(0xfff));
     } else {
 
       if((pt = (pte_t*) kalloc()) == 0) {
@@ -82,5 +83,64 @@ int setupkvm(uint64_t physend) {
   }
 
   lcr3(V2P(pml4));
-  return 1;
+
+  return pml4;
+}
+
+void create_mapping(pte_t* pml4, uint64_t va, uint64_t pa, uint32_t perm) {
+  pte_t *pdpt, *pdt, *pt;
+  pte_t *pml4e, *pdpte, *pde, *pte;
+
+  // Level 1. Map the PDPT entry in PML4E
+  pml4e = &pml4[PML4_INDEX(va)];
+  if (*pml4e & PTE_P) {
+    pdpt = (pte_t*) P2V(*pml4e & ~(0xfff));
+  } else {
+
+    if ((pdpt = (pte_t*) kalloc() ) == 0) {
+      panic("Error allocating PDPT\n"); // error allocating a page for the PDPT
+    }
+
+    memset(pdpt, 0, PGSIZE); // kalloc does this, but just to be sure
+    *pml4e = V2P(pdpt) | PTE_P | perm;
+  }
+
+  // Level 2. Map the PDT entry in PDPT
+  pdpte = &pdpt[PDPT_INDEX(va)];
+  if (*pdpte & PTE_P) {
+    pdt = (pte_t*) P2V(*pdpte & ~(0xfff));
+  } else {
+
+    if((pdt = (pte_t*) kalloc()) == 0) {
+      panic("Error allocating PDT\n"); // error allocting a page for the PDT
+    }
+
+    memset(pdt, 0, PGSIZE); // kalloc does this, but just to tbe sure
+    *pdpte = V2P(pdt) | PTE_P | perm;
+  }
+
+  // Level 3. Map the PT entry in PDT
+  pde = &pdt[PDT_INDEX(va)];
+  if (*pde & PTE_P) {
+    pt = (pte_t*) P2V(*pde & ~(0xfff));
+  } else {
+
+    if((pt = (pte_t*) kalloc()) == 0) {
+      panic("Error allocating PT\n"); // error allocating a page for the PT
+    }
+
+    memset(pt, 0, PGSIZE); // kalloc does this, but just to be sure
+    *pde = V2P(pt) | PTE_P | perm;
+  }
+
+  // Level 4. Map the physical address in PT
+  pte = &pt[PT_INDEX(va)];
+
+  if (*pte & PTE_P) {
+    panic("Mapping exists\n"); // Madness, remapping? Something bad has happened
+  }
+
+  *pte = pa | PTE_P | PTE_U | PTE_W;
+
+  //lcr3(V2P(pml4));
 }
