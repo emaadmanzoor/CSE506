cac3f7702690b544676d2828caf3b1e02ab28c7a
Emaad Ahmed Manzoor <emaadmanzoor@gmail.com>
2015-12-03 20:04:06 -0500

    working user malloc

--- bin/hello/hello.c
+++ bin/hello/hello.c
@@ -2,25 +2,37 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[], char* envp[]) {
-  //while(1) {
-  //  yield();
-  //}
   int x, y;
+  void *v = malloc(1);
+  *(char*) v = 0x42;
   x = fork();
-  if (x==0) {
+  if (x == 0) {
+    // child p->x
     y = fork();
-    if ( y > 0 ){
-      while(1) {
-	yield();
+    if (y > 0) {
+      // child p->x
+      while (1) {
+        v = malloc(200);
+        yield();
+        free(v);
       }
+    } else {
+      // grandchild p->x->y
+      exit(1);
     }
   } else {
+    // parent p
     y = fork();
-    if ( y > 0 ){
+    if (y > 0) {
+      // parent p
       while(1) {
-	yield();
+        yield();
       }
+    } else {
+      // child p->y
+      exit(1);
     }
   }
-  return y;
+  free(v);
+  return 1;
 }
--- libc/mm.c
+++ libc/mm.c
@@ -1,110 +1,116 @@
 #include <stdlib.h>
 
-/*
- * First-fit (circular) linked-list memory allocator
- * Ref: K&R section 8.7, adapted here for x86_64 by
- * aligning data to 8 bytes
+/* First-fit circular linked list buddy allocator, based on K&R section 8.7.
  */
 
-typedef long long Align; // 8 bytes
-
-union header {
-  struct {
-    union header *next;
-    size_t size;
-  } s;
-  Align x;
+struct mheader {
+  struct mheader *next;
+  uint64_t sz;
 };
+typedef struct mheader mheader_t;
 
-typedef union header Header;
+// initial head of the list
+// (the static here is REQUIRED)
+static mheader_t mh;
+static mheader_t *freelist; // pointer to current freelist location
 
-static Header* morecore(unsigned int nu);
+void *malloc(uint64_t sz) {
+  mheader_t *cur, *prev; // loop pointers
+  mheader_t *temp;
+  uint64_t blksz;
 
-static Header base;
-static Header *freep;
-
-void *malloc(size_t nbytes) {
-  Header *p, *prevp; // increments will be in sizeof(Header)
-  unsigned int nunits;
-
-  // new memory is claimed from the OS in multiples of
-  // the header size, which is a multiple of 8 bytes,
-  // so the total size will be aligned to 8 bytes.
-  nunits = (nbytes + sizeof(Header) - 1)/sizeof(Header) + 1;
-
-  prevp = freep;
-  if (freep == NULL) { // first time malloc is run
-    base.s.next = &base;
-    base.s.size = 0;
-    freep = &base;
-    prevp = &base;
+  if (freelist == NULL) {
+    freelist = (mheader_t *) &mh;
+    mh.next = (mheader_t *) &mh;
+    prev = (mheader_t *) &mh;
+    mh.sz = 0;
   }
 
-  // traverse the circular linked list looking for a usable block
-  for (p = prevp->s.next; /* no condition */; prevp = p, p = p->s.next) {
+  // Nice trick from K&R:
+  //  measure sz in units of sizeof(mheader)
+  //  this makes sure the alignment satisfies every data type,
+  //  and makes it convenient to move the header when a block is split.
+  sz = (sz + sizeof(mheader_t) - 1) / sizeof(mheader_t); // how many header sizes
+  sz += 1; // one header-size for the header itself
 
-    // a usable block is found on the list
-    if (p->s.size >= nunits) {
-      if (p->s.size == nunits) { // the block size matches exactly
-        prevp->s.next = p->s.next; // unlink the block
-      } else { // the block size is bigger, split it
-        p->s.size -= nunits;
-        p += p->s.size;
-        p->s.size = nunits;
+  // loops until it returns NULL or an address
+  for (cur = prev->next; /* no condition */; prev = cur, cur = cur->next) {
+    if (cur->sz < sz) {
+      // check if end of the list
+      if (cur == freelist) { // looped around
+        // allocate in chunks
+        blksz = sz; // measure in units of sizeof(mheader)
+        if (blksz < 32) { // 32 * sizeof(mheader) = 4096
+          blksz = 32;
+        }
+
+        // ask the kernel for memory
+        temp = (mheader_t *) sbrk(blksz * sizeof(mheader_t));
+        if (temp == NULL) { // out of memory
+          return NULL;
+        }
+
+        // add the new block to the free list
+        temp->sz = blksz;
+        free((void*)(temp + 1)); // &temp + sizeof(mheader) bytes
+        cur = freelist; // free changed *freelist to point to the new block
       }
-      freep = prevp; // list start is moved to the last seen block
-      return (void *)(p + 1); // skip the header block
-    }
+    } else if (cur->sz == sz) {
+      prev->next = cur->next; // unlink the block
+      freelist = prev; // repoint last touched block
+      return (void *)(cur + 1); // cur + sizeof(header), to skip header
+    } else if (cur->sz > sz) {
+      cur->sz -= sz;
+      cur += cur->sz; // now at the newly split block
+      cur->sz = sz;
 
-    // end of the list, no usable block found
-    if (p == freep) {
-      p = morecore(nunits);
-      if (p == NULL)
-        return NULL;
-      // morecore calls free, returns ptr to the new block
-      // on success (and also modifies freep to point to it)
+      freelist = prev;
+      return (void *)(cur + 1);  // skip the header
     }
   }
 }
 
-void free(void *ap) {
-  Header *bp, *p;
+void free(void *a) {
+  mheader_t *mblk = (mheader_t *) a - 1;
+  mheader_t *prev = freelist;
+  mheader_t *cur = freelist->next;
 
-  bp = (Header*) ap - 1; // point to the start of the block
-  for (p = freep; !(bp > p && bp < p->s.next); p = p->s.next)
-    if (p >= p->s.next && (bp > p || bp < p->s.next))
+  // find a place to insert this block
+  // (blocks are in increasing order of address)
+  while (1) {
+    if (prev >= cur) { // end of list
+      if (mblk > prev || mblk < cur) {
+        // new block lies at the start or end of the list
+        break;
+      }
+    }
+    if (prev < mblk && mblk < cur) {
+      // new block lies in the list between two existing blocks
       break;
-
-  if (bp + bp->s.size == p->s.next) { // next block can be fused
-    bp->s.size += p->s.next->s.size;
-    bp->s.next = p->s.next->s.next;
-  } else {
-    bp->s.next = p->s.next;
+    }
+    prev = cur;
+    cur = cur->next;
   }
 
-  if (p + p->s.size == bp) { // prev block can be fused
-    p->s.size += bp->s.size;
-    p->s.next = bp->s.next;
+  // now place the block in between prev and cur,
+  // fuse the block with its prev/cur if possible.
+
+  // 1. check if mblk can be fused with the next block
+  if (mblk + mblk->sz == cur) {
+    mblk->sz += cur->sz;
+    mblk->next = cur->next; // partially unlink cur
   } else {
-    p->s.next = bp;
+    mblk->next = cur;
   }
 
-  freep = p;
-}
+  // 2. check if mblk can be fused with the previous block
+  if (prev + prev->sz == mblk) {
+    prev->sz += mblk->sz;
+    prev->next = mblk->next;
+  } else {
+    prev->next = mblk;
+  }
 
-static Header *morecore(unsigned int nu) {
-  void *p;
-  Header *up;
-
-  if (nu < 4096) // allocate in chunks to avoid sbrk calls
-    nu = 4096;
-
-  p = sbrk(nu * sizeof(Header));
-  if (p == (void *)-1)
-    return NULL;
-
-  up = (Header *) p;
-  up->s.size = nu;
-  free((void *)(up + 1));
-  return freep;
+  // 3. update the freelist pointer so freelist->next = new block
+  freelist = prev;
 }
